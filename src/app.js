c97193faf781c884abf86c6b318b17ad7b906316
Javier Diez <javierdiez92@gmail.com>
2024-05-13 10:54:28 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+let who_random = Math.floor(Math.random() * who.length);
+let action_random = Math.floor(Math.random() * action.length);
+let what_random = Math.floor(Math.random() * what.length);
+let when_random = Math.floor(Math.random() * when.length);
+
+function excuseGenerator() {
+  return `${who[who_random]} ${action[action_random]} ${what[what_random]} ${when[when_random]}`;
+}
+
+excuse.innerHTML = excuseGenerator();
